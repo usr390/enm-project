cf8c421e4cdd04ed5d73a245bee44e6841d5696b
usr390 <edutre1999@gmail.com>
2023-07-21 23:36:50 -0500

    ENM-53. updated filter s.t. PrimeNG components are used

--- enm/src/app/enm-events/enm-events.module.ts
+++ enm/src/app/enm-events/enm-events.module.ts
@@ -1,16 +1,20 @@
-import { CommonModule } from '@angular/common';
 import { EnmEventListComponent } from './enm-event-list/enm-event-list.component';
 import { EnmEventPageComponent } from './enm-event-page/enm-event-page.component';
 import { EnmEventListFilterComponent } from './enm-event-list-filter/enm-event-list-filter.component';
 import { EnmEventListToolbarComponent } from './enm-event-list-toolbar/enm-event-list-toolbar.component';
 import { EnmEventAddModule } from './enm-event-add/enm-event-add.module';
 import { EnmEventRoutingModule } from './enm-events-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
-import { SharedModule } from './../shared/shared.module';
 import { EnmEventTimeComponent } from './enm-event-add/enm-event-time/enm-event-time.component';
 import { EnmEventPriceOfEntryComponent } from './enm-event-add/enm-event-price-of-entry/enm-event-price-of-entry.component';
 import { EnmEventArtistsComponent } from './enm-event-add/enm-event-artists/enm-event-artists.component';
+
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { SharedModule } from './../shared/shared.module';
+
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 
 
@@ -25,12 +29,14 @@ import { EnmEventArtistsComponent } from './enm-event-add/enm-event-artists/enm-
     EnmEventArtistsComponent,
   ],
   imports: [
-    CommonModule,
     EnmEventAddModule,
     EnmEventRoutingModule,
+    CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    SharedModule
+    SharedModule,
+    ButtonModule,
+    InputTextModule
   ],
   exports: [
     EnmEventListComponent,
