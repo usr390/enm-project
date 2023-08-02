127d88295a2adf51a04fb571f64096be65964268
usr390 <edutre1999@gmail.com>
2023-08-01 20:58:47 -0500

    ENM-59. some separation of concerns for a couple modules
    somehow during development the two modules mixed components. made sure to allocate components to their proper module
    
    also, just a general makeover to both files (adding some comments, organizing imports, etc)

--- enm/src/app/enm-events/enm-event-add/enm-event-add.module.ts
+++ enm/src/app/enm-events/enm-event-add/enm-event-add.module.ts
@@ -8,24 +8,28 @@ import { RouterModule } from '@angular/router';
 import { EnmEventAddRoutingModule } from './enm-event-add-routing.module';
 import { EnmEventAddressComponent } from './enm-event-address/enm-event-address.component';
 import { EnmEventDateComponent } from './enm-event-date/enm-event-date.component';
-import { SharedModule } from 'src/app/shared/shared.module';
-
+import { EnmEventTimeComponent } from './enm-event-time/enm-event-time.component';
+import { EnmEventPriceOfEntryComponent } from './enm-event-price-of-entry/enm-event-price-of-entry.component';
+import { EnmEventArtistsComponent } from './enm-event-artists/enm-event-artists.component';
+import { SharedModule } from './../../shared/shared.module';
 
 @NgModule({
   declarations: [
     EnmEventAddressComponent,
-    EnmEventDateComponent
+    EnmEventDateComponent,
+    EnmEventTimeComponent,
+    EnmEventPriceOfEntryComponent,
+    EnmEventArtistsComponent,
   ],
   imports: [
+    // angular modules
     CommonModule,
     ReactiveFormsModule,
     RouterModule,
+    // enm modules
     EnmEventAddRoutingModule,
     SharedModule
   ],
-  exports: [
-    EnmEventAddressComponent,
-    EnmEventDateComponent
-  ]
+  exports: []
 })
 export class EnmEventAddModule { }
