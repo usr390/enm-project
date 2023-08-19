6c8088ed3ec5896fef1bd8c47e0ec0c852fc2f7b
usr390 <edutre1999@gmail.com>
2023-08-18 20:17:39 -0500

    ENM-77. corrected failing test on EEAddVenueComponent
    i think when components get on the complicated side (i.e. start importing lots of things) the default .spec scaffoling starts to fail as it doesn't keep up with changes on the component
    for this change i added some of the dependencies the spec file was missing

--- enm/src/app/enm-events/enm-event-add/enm-event-add-venue/enm-event-add-venue.component.spec.ts
+++ enm/src/app/enm-events/enm-event-add/enm-event-add-venue/enm-event-add-venue.component.spec.ts
@@ -1,15 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EnmEventAddVenueComponent } from './enm-event-add-venue.component';
+import { EnmEventAddMultipageFormService } from 'src/app/core/services/enm-event-add-multipage-form.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('EnmEventAddVenueComponent', () => {
   let component: EnmEventAddVenueComponent;
   let fixture: ComponentFixture<EnmEventAddVenueComponent>;
+  let mockEnmEventAddMultipageFormService: jasmine.SpyObj<EnmEventAddMultipageFormService>;
 
   beforeEach(() => {
+    mockEnmEventAddMultipageFormService = jasmine.createSpyObj('EnmEventAddMultipageFormService', ['']);
     TestBed.configureTestingModule({
-      declarations: [EnmEventAddVenueComponent]
-    });
+      imports: [ HttpClientTestingModule ], 
+      declarations: [ EnmEventAddVenueComponent ],
+      providers: [ EnmEventAddMultipageFormService ] 
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(EnmEventAddVenueComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
