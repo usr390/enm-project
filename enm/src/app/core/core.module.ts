6f4a4d76cfe07140df9204603125a9c26e09bb3d
usr390 <edutre1999@gmail.com>
2023-09-04 18:18:09 -0500

    ENM-91. add link to LoginComp in SidebarComp

--- enm/src/app/core/core.module.ts
+++ enm/src/app/core/core.module.ts
@@ -1,14 +1,14 @@
+// angular imports
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-
-import { SidebarComponent } from './sidebar/sidebar.component';
-
+import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 // enm imports
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
@@ -17,9 +17,11 @@ import { LoginComponent } from './login/login.component';
     LoginComponent,
   ],
   imports: [
+    // angular modules
     CommonModule,
     HttpClientModule,
     ReactiveFormsModule,
+    RouterModule,
     // enm modules,
     SharedModule,
   ],
