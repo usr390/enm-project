7c83b22614b0016990d8e9828d5d128b2c8cdccd
usr390 <edutre1999@gmail.com>
2023-09-04 17:32:23 -0500

    ENM-91. LoginComp redirects to EEListComp on successful login

--- enm/src/app/core/services/login.service.ts
+++ enm/src/app/core/services/login.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { User } from 'src/app/models/user.model';
+import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 const BASE_URL = environment.api + '/login';
 
@@ -12,11 +12,12 @@ export class LoginService {
 
   public userLoginForm: FormGroup = this.fb.group({});
 
-  constructor(private fb: FormBuilder, private http: HttpClient) { }
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) { }
 
   postLogin() {
     this.http.post(BASE_URL, this.userLoginForm.value)
     .subscribe({
+      next: _ => this.router.navigate(['/']),
       error: error => console.log(error),
     });
   }
