802bf6de3427b14ae3edc76e51d4243822a84398
usr390 <edutre1999@gmail.com>
2023-09-04 20:39:18 -0500

    ENM-91. updated .spec files to account for module/service deps

--- enm/src/app/core/sidebar/sidebar.component.spec.ts
+++ enm/src/app/core/sidebar/sidebar.component.spec.ts
@@ -1,19 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SidebarComponent } from './sidebar.component';
+import { LoginService } from '../services/login.service';
+import { CoreModule } from '../core.module';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
+  let mockLoginService: jasmine.SpyObj<LoginService>;
 
   beforeEach(() => {
+    mockLoginService = jasmine.createSpyObj('LoginService', ['']);
     TestBed.configureTestingModule({
-      declarations: [SidebarComponent]
-    });
+      imports: [ CoreModule ],
+      declarations: [ SidebarComponent ],
+      providers: [ LoginService ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
