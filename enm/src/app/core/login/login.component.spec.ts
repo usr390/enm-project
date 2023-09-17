ab9e94e7c4f3afc0db07e52b634e8e69b1fc875c
vendingcompute <edutre1999@gmail.com>
2023-09-16 21:34:19 -0500

    ENM-91. fixed failing test related to 5e94900
    Included a new service in the component so needs to be reflected in the spec file

--- enm/src/app/core/login/login.component.spec.ts
+++ enm/src/app/core/login/login.component.spec.ts
@@ -5,20 +5,26 @@ import { LoginComponent } from './login.component';
 import { LoginService } from '../services/login.service';
 import { of } from 'rxjs';
 import { Store } from '@ngrx/store';
+import { MessageService } from 'primeng/api';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
   let mockLoginService: jasmine.SpyObj<LoginService>;
+  let mockMessageService: jasmine.SpyObj<MessageService>;
 
   const mockStore = { select: jasmine.createSpy().and.returnValue(of(null)), };
 
   beforeEach(() => {
     mockLoginService = jasmine.createSpyObj('LoginService', ['']);
+    mockMessageService = jasmine.createSpyObj('MessageService', ['']);
     TestBed.configureTestingModule({
       imports: [ HttpClientTestingModule ],
       declarations: [ LoginComponent ],
-      providers: [{ provide: Store, useValue: mockStore }]
+      providers: [
+        { provide: Store, useValue: mockStore },
+        { provide: MessageService, useValue: mockMessageService },
+      ]
     }).compileComponents();
   });
 
