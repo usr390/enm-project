ec3f830dc5598ecad293542db9a57736fb2592f4
vendingcompute <edutre1999@gmail.com>
2023-09-18 23:30:13 -0500

    ENM-95. renamed folder containing EECoverComp
    Renamed to 'enm-event-cover'. forgot to do this in previous commit (981994a)

--- enm/src/app/enm-events/enm-event-add/enm-event-add-routing.module.ts
+++ enm/src/app/enm-events/enm-event-add/enm-event-add-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { EnmEventVenueComponent } from './enm-event-venue/enm-event-venue.component';
 import { EnmEventDateComponent } from './enm-event-date/enm-event-date.component';
 import { EnmEventTimeComponent } from './enm-event-time/enm-event-time.component';
-import { EnmEventCoverComponent } from './enm-event-price-of-entry/enm-event-cover.component';
+import { EnmEventCoverComponent } from './enm-event-cover/enm-event-cover.component';
 import { EnmEventArtistsComponent } from './enm-event-artists/enm-event-artists.component';
 import { EnmEventAddVenueCityComponent } from './enm-event-add-venue-city/enm-event-add-venue-city.component';
 import { EnmEventAddVenueAddressComponent } from './enm-event-add-venue-address/enm-event-add-venue-address.component';
--- enm/src/app/enm-events/enm-event-add/enm-event-add.module.ts
+++ enm/src/app/enm-events/enm-event-add/enm-event-add.module.ts
@@ -9,7 +9,7 @@ import { EnmEventAddRoutingModule } from './enm-event-add-routing.module';
 import { EnmEventVenueComponent } from './enm-event-venue/enm-event-venue.component';
 import { EnmEventDateComponent } from './enm-event-date/enm-event-date.component';
 import { EnmEventTimeComponent } from './enm-event-time/enm-event-time.component';
-import { EnmEventCoverComponent } from './enm-event-price-of-entry/enm-event-cover.component';
+import { EnmEventCoverComponent } from './enm-event-cover/enm-event-cover.component';
 import { EnmEventArtistsComponent } from './enm-event-artists/enm-event-artists.component';
 import { SharedModule } from './../../shared/shared.module';
 import { EnmEventAddVenueCityComponent } from './enm-event-add-venue-city/enm-event-add-venue-city.component';
