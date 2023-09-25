59cff6cfb001c553e9ba70abed6b479bb4265f44
vendingcompute <edutre1999@gmail.com>
2023-09-24 19:17:16 -0500

    removed LogInService artifacts from previous implementations

--- enm/src/app/core/services/login.service.ts
+++ enm/src/app/core/services/login.service.ts
@@ -13,20 +13,7 @@ export class LoginService {
 
   public userLoginForm: FormGroup = this.fb.group({});
 
-  user = '';
-
   constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) { }
-
-  postLogin() {
-    this.http.post(BASE_URL, this.userLoginForm.value)
-    .subscribe({
-      next: _ => { 
-        this.user = this.userLoginForm.value.username; 
-        this.router.navigate(['/']);
-      },
-      error: error => console.log(error),
-    });
-  }
 
   login(username: string, password: string) {
     return this.http.post<LogInSuccessResponse>(BASE_URL, { username, password });
