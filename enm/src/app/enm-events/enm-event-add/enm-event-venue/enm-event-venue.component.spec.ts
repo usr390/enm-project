c522c75414191e2786330762fee72c0e5ca8383b
vendingcompute <edutre1999@gmail.com>
2023-10-04 19:05:35 -0500

    added necessary dependencies to EnmEventVenueComp's spec
    Since I'm dep injecting store into this component it also needs refs in its spec file

--- enm/src/app/enm-events/enm-event-add/enm-event-venue/enm-event-venue.component.spec.ts
+++ enm/src/app/enm-events/enm-event-add/enm-event-venue/enm-event-venue.component.spec.ts
@@ -3,18 +3,23 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { EnmEventVenueComponent } from './enm-event-venue.component';
 import { EnmEventAddMultipageFormService } from '../../../core/services/enm-event-add-multipage-form.service'; 
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
 
 describe('EnmEventAddressComponent', () => {
   let component: EnmEventVenueComponent;
   let fixture: ComponentFixture<EnmEventVenueComponent>;
   let mockEnmEventAddMultipageFormService: jasmine.SpyObj<EnmEventAddMultipageFormService>;
+  const mockStore = { select: jasmine.createSpy().and.returnValue(of(null)), };
 
   beforeEach(() => {
     mockEnmEventAddMultipageFormService = jasmine.createSpyObj('EnmEventAddMultipageFormService', ['']);
     TestBed.configureTestingModule({
       imports: [ HttpClientTestingModule ], 
       declarations: [ EnmEventVenueComponent ],
-      providers: [ EnmEventAddMultipageFormService ] 
+      providers: [ 
+        EnmEventAddMultipageFormService,
+        { provide: Store, useValue: mockStore } ] 
     }).compileComponents();
   });
 
