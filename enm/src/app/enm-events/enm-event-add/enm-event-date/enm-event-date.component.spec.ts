ed6ef7d5d3dbb4a42a83c70d03c94bd210095e72
vendingcompute <edutre1999@gmail.com>
2023-10-12 19:15:36 -0500

    ENM-102. fixed failing EEDateComp tests

--- enm/src/app/enm-events/enm-event-add/enm-event-date/enm-event-date.component.spec.ts
+++ enm/src/app/enm-events/enm-event-add/enm-event-date/enm-event-date.component.spec.ts
@@ -3,18 +3,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EnmEventDateComponent } from './enm-event-date.component';
 import { EnmEventAddMultipageFormService } from './../../../core/services/enm-event-add-multipage-form.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
 
 describe('EnmEventDateComponent', () => {
   let component: EnmEventDateComponent;
   let fixture: ComponentFixture<EnmEventDateComponent>;
   let mockEnmEventAddMultipageFormService: jasmine.SpyObj<EnmEventAddMultipageFormService>;
+  const mockStore = { select: jasmine.createSpy().and.returnValue(of(null)), };
 
   beforeEach(() => {
     mockEnmEventAddMultipageFormService = jasmine.createSpyObj('EnmEventAddMultipageFormService', ['']);
     TestBed.configureTestingModule({
       imports: [ HttpClientTestingModule ], 
       declarations: [ EnmEventDateComponent ],
-      providers: [ EnmEventAddMultipageFormService ] 
+      providers: [ 
+        EnmEventAddMultipageFormService,
+        { provide: Store, useValue: mockStore } ] 
     }).compileComponents();
   });
 
