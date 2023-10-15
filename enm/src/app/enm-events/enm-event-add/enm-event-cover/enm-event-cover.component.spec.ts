5a18e46d97fbb1c0c62c3008a77a0a1f788a49fe
vendingcompute <edutre1999@gmail.com>
2023-10-14 22:22:18 -0500

    ENM-102. added form saving mechanism to EECoverComp

--- enm/src/app/enm-events/enm-event-add/enm-event-cover/enm-event-cover.component.spec.ts
+++ enm/src/app/enm-events/enm-event-add/enm-event-cover/enm-event-cover.component.spec.ts
@@ -3,18 +3,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EnmEventCoverComponent } from './enm-event-cover.component';
 import { EnmEventAddMultipageFormService } from '../../../core/services/enm-event-add-multipage-form.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
 
 describe('EnmEventCoverComponent', () => {
   let component: EnmEventCoverComponent;
   let fixture: ComponentFixture<EnmEventCoverComponent>;
   let mockEnmEventAddMultipageFormService: jasmine.SpyObj<EnmEventAddMultipageFormService>;
+  const mockStore = { select: jasmine.createSpy().and.returnValue(of(null)), };
 
   beforeEach(() => {
     mockEnmEventAddMultipageFormService = jasmine.createSpyObj('EnmEventAddMultipageFormService', ['']);
     TestBed.configureTestingModule({
       imports: [ HttpClientTestingModule ], 
       declarations: [ EnmEventCoverComponent ],
-      providers: [ EnmEventAddMultipageFormService ] 
+      providers: [ 
+        EnmEventAddMultipageFormService,
+        { provide: Store, useValue: mockStore } 
+      ] 
     }).compileComponents();
   });
 
--- enm/src/app/enm-events/enm-event-add/enm-event-cover/enm-event-cover.component.ts
+++ enm/src/app/enm-events/enm-event-add/enm-event-cover/enm-event-cover.component.ts
@@ -3,6 +3,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { EnmEventAddMultipageFormService } from '../../../core/services/enm-event-add-multipage-form.service';
+import { Store } from '@ngrx/store';
+
+import * as AuthActions from '../../../state/auth/auth.actions';
+import * as fromAuth from './../../../state/auth/auth.reducer';
+import { take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-enm-event-cover',
@@ -16,11 +21,17 @@ export class EnmEventCoverComponent {
   */
 
   enmEventAddForm: FormGroup = this.enmEventAddMultipageFormService.enmEventAddMultipageForm;
+  enmEventAddFormValuesActionStream$ = this.enmEventAddMultipageFormService.enmEventAddMultipageForm.valueChanges.pipe(
+    tap(value => { this.store$.dispatch(AuthActions.updateForm({ formValue: value })) }),
+  );
 
-  constructor(private enmEventAddMultipageFormService: EnmEventAddMultipageFormService, private fb: FormBuilder, private router: Router) {}
+  selectedCover$ = this.store$.select(fromAuth.selectCover);
+
+  constructor(private store$: Store, private enmEventAddMultipageFormService: EnmEventAddMultipageFormService, private fb: FormBuilder, private router: Router) {}
 
   ngOnInit() {
     this.setUpLocalFormControls();
+    this.initializeFormControls();
   }
 
   onSubmit() {
@@ -42,7 +53,6 @@ export class EnmEventCoverComponent {
     this.enmEventAddForm.setControl('cover', this.fb.control('', [Validators.required]));
   }
   tearDownLocalFormControls() {
-    this.enmEventAddForm.removeControl('cover');
   }
   tearDownExistingFormControls() {
     this.enmEventAddForm.removeControl('cover');
@@ -56,6 +66,12 @@ export class EnmEventCoverComponent {
     let coverWithoutCurrencySymbol = cover.slice(1, cover.length)
     let coverAsNumber = parseInt(coverWithoutCurrencySymbol);
     this.enmEventAddForm.get('cover')?.patchValue(coverAsNumber);
+  }
+  initializeFormControls() {
+    // only initializes if there's cover-related state in NgRx store
+    this.selectedCover$.pipe(take(1)).subscribe(cover => {
+      this.enmEventAddForm.get('cover')?.setValue('$' + cover?.toString())
+    });
   }
   //#endregion
 }
