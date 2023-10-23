6c59ce721dfc095b7c9917d9bad32acf1472107b
vendingcompute <edutre1999@gmail.com>
2023-10-22 19:57:26 -0500

    implemented mock functionality for converting a non plus user into a plus user

--- enm/src/app/core/services/enm-plus-payment.service.ts
+++ enm/src/app/core/services/enm-plus-payment.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { NullableUser } from 'src/app/models/user.model';
 import { environment } from 'src/environments/environment';
 const BASE_URL = environment.api + '/create-checkout-session';
 
@@ -15,4 +16,8 @@ export class EnmPlusPaymentService {
   checkoutSession$ = this.http.post<StripeCheckoutSession>(BASE_URL, {})
 
   constructor(private http: HttpClient) { }
+
+  plusifyUser(user: NullableUser) {
+    console.log(user?.username + ' plusified!')
+  }
 }
--- enm/src/app/enm-events/enm-plus-payment-successful/enm-plus-payment-successful.component.ts
+++ enm/src/app/enm-events/enm-plus-payment-successful/enm-plus-payment-successful.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { EnmPlusPaymentService } from 'src/app/core/services/enm-plus-payment.service';
+import * as fromAuth from './../../state/auth/auth.reducer';
+import { concatMap, map, take } from 'rxjs';
+
 
 @Component({
   selector: 'app-enm-plus-payment-successful',
@@ -6,5 +11,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./enm-plus-payment-successful.component.less']
 })
 export class EnmPlusPaymentSuccessfulComponent {
+
+  user$ = this.store$.select(fromAuth.selectUser).pipe(
+    take(1), 
+    map((user) => this.enmPlusPaymentService.plusifyUser(user) )
+  )
+
+  constructor(private store$: Store, private enmPlusPaymentService: EnmPlusPaymentService) {}
 
 }
