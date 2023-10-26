4335950e95b64901090894dabb9fdd190671f400
vendingcompute <edutre1999@gmail.com>
2023-10-26 18:56:02 -0500

    ENM-110. deleted payment state files from app state

--- enm/src/app/state/app.state.ts
+++ enm/src/app/state/app.state.ts
@@ -1,9 +1,7 @@
 import { AuthState } from "./auth/auth.reducer";
 import { FormState } from "./form/form.reducer";
-import { PaymentState } from "./payment/payment.reducer";
 
 export interface AppState {
     auth: AuthState;
     form: FormState;
-    payment: PaymentState
 }
