563157aa7914c14b87123edc3d76ee6e3465a469
vendingcompute <edutre1999@gmail.com>
2023-10-29 15:03:58 -0500

    made searchbar a plus-only feature
    also some light rewording on payment screen

--- enm/src/app/enm-events/enm-event-list-toolbar/enm-event-list-toolbar.component.ts
+++ enm/src/app/enm-events/enm-event-list-toolbar/enm-event-list-toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromAuth from './../../state/auth/auth.reducer';
 
 @Component({
   selector: 'app-enm-event-list-toolbar',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./enm-event-list-toolbar.component.less']
 })
 export class EnmEventListToolbarComponent {
+
+  user$ = this.store$.select(fromAuth.selectUser);
+
+  constructor(private store$: Store){}
 
 }
