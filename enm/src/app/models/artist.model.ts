4f00e79ecfb47c2ef89d5e2149ea778de35e4019
vendingcompute <edutre1999@gmail.com>
2023-11-07 22:33:04 -0600

    ENM-124. created artist model
    Will be used to slowly transition artist references to an actual artist type instead of just a string that represents the artist name

---
+++ enm/src/app/models/artist.model.ts
@@ -0,0 +1,12 @@
+
+export interface Artist {
+
+    /* summary
+        artist is an individual, group, band, collective, or other entity that produces music
+    */
+
+    "_id": string,
+    "name": string,
+    "link": string
+
+}
