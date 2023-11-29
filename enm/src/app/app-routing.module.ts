0f6d19d31217975d8a6b66620ffac8c0958defe2
vendingcompute <edutre1999@gmail.com>
2023-11-28 18:34:35 -0600

    Explicit lazy loading on AppRoutingModule
    Hm, not entirely sure if this is doing something. But without this line it doesn't make sense to me how the app is able to figure out how to route to the listing component (EEListComp). Maybe because the component is associated to a blank route ('') and so it defaults to it? Idk it's confusing since I thought somehow AppRoutingModule needed a reference to EnmEventRoutingModule for everything to come together. Either way, explicitly writing down the lazy loading of EnmEvents helps with my mental model of the app's routing logic

--- enm/src/app/app-routing.module.ts
+++ enm/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
+// angular imports
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// enm imports
 import { LogInComponent } from './core/login/login.component';
 import { CreateUserComponent } from './core/create-user/create-user.component';
 
 const routes: Routes = [
   { path: 'login', component: LogInComponent },
   { path: 'create-user', component: CreateUserComponent },
+  { path: '', loadChildren: () => import('./enm-events/enm-events.module').then(m => m.EnmEventModule) },
 ];
 
 @NgModule({
