049d2c1b08d84900a49e4deeeee42d55d73795cd
vendingcompute <edutre1999@gmail.com>
2023-11-28 18:06:09 -0600

    rearranged some stuff in app module

--- enm/src/app/app.module.ts
+++ enm/src/app/app.module.ts
@@ -1,36 +1,43 @@
-// angular imports
-import { NgModule } from '@angular/core';
+// angular module imports
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
 
 // enm imports
 import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
 import { EnmEventModule } from './enm-events/enm-events.module';
-import { AuthEffects } from './state/auth/auth.effects';
-import { PaymentEffects } from './state/payment/payment.effects';
-import { EnmEventsEffects } from './state/enmEvents/enmEvents.effects';
-import { HydrationEffects } from './state/hydration/hydration.effects';
-import { reducers, metaReducers } from './state/app.state';
+import { SharedModule } from './shared/shared.module';
 
 // 3rd party imports
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 import { EffectsModule } from '@ngrx/effects';
+  // written by enm team but related to parent
+  import { AuthEffects } from './state/auth/auth.effects';
+  import { EnmEventsEffects } from './state/enmEvents/enmEvents.effects';
+  import { HydrationEffects } from './state/hydration/hydration.effects';
+  import { PaymentEffects } from './state/payment/payment.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { StoreModule } from '@ngrx/store';
+  // written by enm team but related to parent
+  import { reducers, metaReducers } from './state/app.state';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    //#region angular modules
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    //#endregion 
+    //#region enm modules
     CoreModule,
-    SharedModule,
     EnmEventModule,
+    SharedModule,
+    //#endregion
+    //#region 3rd party modules
     StoreModule.forRoot(reducers, {metaReducers}),
     StoreDevtoolsModule.instrument(),
     EffectsModule.forRoot([ 
@@ -39,6 +46,7 @@ import { EffectsModule } from '@ngrx/effects';
       EnmEventsEffects,
       HydrationEffects
     ]),
+    //#endregion
   ],
   bootstrap: [AppComponent]
 })
