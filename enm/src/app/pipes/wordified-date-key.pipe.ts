36610dadc63df50e7188c52c7b9677f598143419
vendingcompute <edutre1999@gmail.com>
2023-12-28 12:06:29 -0600

    Removed month and day from weekly events
    Should help discern this weeks events from all other events. Makes it easier to know what is most relevant

--- enm/src/app/pipes/wordified-date-key.pipe.ts
+++ enm/src/app/pipes/wordified-date-key.pipe.ts
@@ -6,37 +6,53 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class WordifiedDateKeyPipe implements PipeTransform {
 
   transform(dateKey: string): string {
-    // format should be exactly 'yyyymmdd', return empty string if not
-    if (dateKey.length !== 8) return '';
+// format should be exactly 'yyyymmdd', return empty string if not
+  if (dateKey.length !== 8) return '';
 
-    const year = parseInt(dateKey.slice(0, 4));
-    const month = parseInt(dateKey.slice(4, 6)) - 1; // Adjust month index (-1)
-    const day = parseInt(dateKey.slice(6, 8));
-    const date = new Date(year, month, day);
+  const year = parseInt(dateKey.slice(0, 4));
+  const month = parseInt(dateKey.slice(4, 6)) - 1; // Adjust month index (-1)
+  const day = parseInt(dateKey.slice(6, 8));
+  const eventDate = new Date(year, month, day);
 
-    // Create a new Date object for today's date
-    const today = new Date();
-    today.setHours(0, 0, 0, 0); // Reset time to start of the day
+  // Reset time to start of the day for accurate comparisons
+  eventDate.setHours(0, 0, 0, 0);
 
-    // Create a new Date object for tomorrow's date
-    const tomorrow = new Date(today);
-    tomorrow.setDate(tomorrow.getDate() + 1);
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
 
-    // Check if the given date is 'today'
-    if (date.getTime() === today.getTime()) {
-      return 'Today';
-    }
+  const tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 1);
 
-    // Check if the given date is 'tomorrow'
-    if (date.getTime() === tomorrow.getTime()) {
-      return 'Tomorrow';
-    }
-
-    // Format the date
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const monthsOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    
-    return `${daysOfWeek[date.getDay()]}, ${monthsOfYear[date.getMonth()]} ${date.getDate()}`;
+  // Check if the given date is 'today' or 'tomorrow'
+  if (eventDate.getTime() === today.getTime()) {
+    return 'Today';
+  } else if (eventDate.getTime() === tomorrow.getTime()) {
+    return 'Tomorrow';
   }
+
+  // Calculate start and end of the current week (Monday to Sunday)
+  const currentDay = today.getDay();
+  const daysToLastMonday = (currentDay + 6) % 7; // 0 is Sunday in getDay()
+  const daysToNextSunday = 7 - currentDay;
+
+  const weekStart = new Date(today);
+  weekStart.setDate(today.getDate() - daysToLastMonday);
+
+  const weekEnd = new Date(today);
+  weekEnd.setDate(today.getDate() + daysToNextSunday);
+
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const monthsOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  // Check if the date is within this week
+  if (eventDate >= weekStart && eventDate <= weekEnd) {
+    // If it's this week, just return the day of the week
+    return daysOfWeek[eventDate.getDay()];
+  } else {
+    // If it's beyond this week, return the full format
+    return `${daysOfWeek[eventDate.getDay()]}, ${monthsOfYear[eventDate.getMonth()]} ${eventDate.getDate()}`;
+  }
+}
+
 
 }
