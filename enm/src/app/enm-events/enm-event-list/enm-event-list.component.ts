b160b82053232d2035100aa4afd579a0e97c6d16
vendingcompute <edutre1999@gmail.com>
2024-01-02 19:41:00 -0600

    ENM-176. Touch to copy address

--- enm/src/app/enm-events/enm-event-list/enm-event-list.component.ts
+++ enm/src/app/enm-events/enm-event-list/enm-event-list.component.ts
@@ -1,5 +1,5 @@
 // angular
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 // 3rd party
 import { Store } from '@ngrx/store';
 import { filter, map, take } from 'rxjs';
@@ -10,6 +10,7 @@ import { AppState } from 'src/app/state/app.state';
 import * as AuthSelectors from './../../state/auth/auth.selectors';
 import * as fromEnmEvent from './../../state/enmEvents/enmEvents.selectors';
 import * as enmEventsActions from './../../state/enmEvents/enmEvents.actions';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-enm-event-list',
@@ -18,7 +19,11 @@ import * as enmEventsActions from './../../state/enmEvents/enmEvents.actions';
 })
 export class EnmEventListComponent implements OnInit {
 
+  @ViewChild('addressText') addressTextElement!: ElementRef;
+  @ViewChild('venueAddressText') venueAddressElement!: ElementRef;
+
   isVisible: boolean = true;
+  isCopying: boolean = false;
 
   hideElementTemporarily() {
     this.isVisible = false;
@@ -27,7 +32,8 @@ export class EnmEventListComponent implements OnInit {
 
 
   constructor(
-    private store$: Store<AppState>
+    private store$: Store<AppState>,
+    private messageService: MessageService
   ) { }
 
   user$ = this.store$.select(AuthSelectors.selectUser); // for distinguishing between regular and plus users
@@ -62,5 +68,24 @@ export class EnmEventListComponent implements OnInit {
     this.store$.dispatch(enmEventsActions.selectEventFromEventList({_id: _id as string}))
   }
 
+  copyAddress(event: MouseEvent) {
+    event.stopPropagation();
+    this.isCopying = true;
+  
+    let textToCopy = this.addressTextElement?.nativeElement.innerText || this.venueAddressElement?.nativeElement.innerText;
+    if (textToCopy) {
+      navigator.clipboard.writeText(textToCopy).then(() => {
+        this.messageService.add({ key: 'addressCopied', severity: 'success', summary: "Success", detail: 'Address Copied To Clipboard' });
+      }).catch(err => {
+        console.error('Failed to copy: ', err);
+        this.messageService.add({ key: 'addressCopiedError', severity: 'error', summary: "Error", detail: 'Could Not Copy Address To Clipboard' });
+        alert('Unable to copy. Please manually copy the text.');
+      });
+    } else {
+      alert('No address to copy.');
+    }
+  
+    setTimeout(() => this.isCopying = false, 100); 
+  }
 }
 
