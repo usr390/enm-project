bde4deb3979d305277bf8fd49edda1261955eb82
vendingcompute <edutre1999@gmail.com>
2024-01-05 12:36:43 -0600

    Added click-to-copy to EEPageComp
    For plus users when they are on the link page (EEPageComp). They should also be able to copy the address to their clipboard from there too

--- enm/src/app/enm-events/enm-event-page/enm-event-page.component.spec.ts
+++ enm/src/app/enm-events/enm-event-page/enm-event-page.component.spec.ts
@@ -5,11 +5,13 @@ import { EnmEventPageComponent } from './enm-event-page.component';
 import { EnmEventService } from './../../core/services/enm-event.service';
 import { of } from 'rxjs';
 import { Store } from '@ngrx/store';
+import { MessageService } from 'primeng/api';
 
 describe('EnmEventPageComponent', () => {
   let component: EnmEventPageComponent;
   let fixture: ComponentFixture<EnmEventPageComponent>;
   let mockEnmEventService: jasmine.SpyObj<EnmEventService>;
+  let mockMessageService: jasmine.SpyObj<MessageService>;
   const mockStore = { select: jasmine.createSpy().and.returnValue(of(null)), };
 
   beforeEach(async () => {
@@ -21,6 +23,8 @@ describe('EnmEventPageComponent', () => {
       providers: [
         { provide: EnmEventService, useValue: mockEnmEventService },
         { provide: Store, useValue: mockStore },
+        { provide: MessageService, useValue: mockMessageService },
+
 
       ]
     })
--- enm/src/app/enm-events/enm-event-page/enm-event-page.component.ts
+++ enm/src/app/enm-events/enm-event-page/enm-event-page.component.ts
@@ -7,6 +7,7 @@ import { combineLatest, map } from 'rxjs';
 // enm imports
 import { AppState } from 'src/app/state/app.state';
 import * as EnmEventsSelectors from './../../state/enmEvents/enmEvents.selectors';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-enm-event-page',
@@ -17,17 +18,93 @@ export class EnmEventPageComponent implements OnInit {
 
   constructor(
     private store$: Store<AppState>,
-    private router: Router
+    private router: Router,
+    private messageService: MessageService
   ) { }
+
+  lastCopyTimestamp: number = 0;
+  isCopying: boolean = false;
+  isMobile!: boolean;
 
   enmEvent$ = combineLatest([this.store$.select(EnmEventsSelectors.selectAll), this.store$.select(EnmEventsSelectors.selectSelectedEventId)]).pipe(
     map(([enmEvents, spotlightEnmEventId]) => enmEvents.filter(enmEvent => enmEvent._id === spotlightEnmEventId)[0]),
   );
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    this.checkDeviceType()
+  }
 
   goBack() {
     this.router.navigate(['/']); 
+  }
+
+  copyAddress(event: MouseEvent) {
+    event.stopPropagation(); // Prevent navigating to another route
+  
+    // rate limiting logic
+    const now = Date.now();
+    const timeSinceLastCopy = now - this.lastCopyTimestamp;
+    if (timeSinceLastCopy < 2000) {  // 2-second limit
+      return;
+    }
+    this.lastCopyTimestamp = now;
+    this.isCopying = true;
+  
+    // initialize variables to hold the text elements
+    let textToCopy = '';
+    const target = event.target as HTMLElement;
+  
+    // function to recursively search for parent with a specific class
+    function findParentWithClass(element: HTMLElement, className: string): HTMLElement | null {
+      while (element && !element.classList.contains(className)) {
+        if (element.parentElement) {
+          element = element.parentElement;
+        } else {
+          return null;
+        }
+      }
+      return element;
+    }
+  
+    // ensure that the target is an HTMLElement before trying to access innerText
+    if (target) {
+      const clickableArea = findParentWithClass(target, 'click-to-copy');
+  
+      // check if the clicked area or its children have the address classes
+      if (clickableArea) {
+        const addressElement = clickableArea.querySelector('.card-text.address-text');
+        const venueAddressElement = clickableArea.querySelector('.card-text.venue-address-text');
+  
+        if (addressElement && addressElement instanceof HTMLElement) {
+          textToCopy = addressElement.innerText.trim();
+        } else if (venueAddressElement && venueAddressElement instanceof HTMLElement) {
+          textToCopy = venueAddressElement.innerText.trim();
+        }
+      }
+    }
+  
+    // copy the text if found
+    if (textToCopy) {
+      navigator.clipboard.writeText(textToCopy).then(() => {
+        this.messageService.add({ key: 'addressCopied', severity: 'success', summary: "Success", detail: 'Address Copied To Clipboard' });
+      }).catch(err => {
+        console.error('Failed to copy: ', err);
+        this.messageService.add({ key: 'addressCopiedError', severity: 'error', summary: "Error", detail: 'Could Not Copy Address To Clipboard' });
+      });
+    } else {
+      alert('No address to copy.');
+    }
+  
+    // reset the isCopying flag
+    setTimeout(() => this.isCopying = false, 2000); // Reset after a short delay
+  }
+
+  checkDeviceType(){
+    const userAgent = window.navigator.userAgent;
+    const screenWidth = window.innerWidth;
+
+    // very basic way to identify a mobile device. might want to use a more robust approach
+    this.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent) || screenWidth < 768;
   }
 
 }
