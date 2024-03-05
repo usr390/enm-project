a355a9a523b6ec4c0caed516ddb426c8b82b50fd
vendingcompute <edutre1999@gmail.com>
2024-03-04 22:36:25 -0600

    x

--- enm/src/app/artist-directory-guard.guard.ts
+++ enm/src/app/artist-directory-guard.guard.ts
@@ -12,14 +12,12 @@ import * as AuthSelectors from './state/auth/auth.selectors';
 export class ArtistDirectoryGuardService implements CanActivate {
   constructor(private store: Store<AppState>, private router: Router) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+  canActivate(): Observable<boolean> {
     console.log('Guard activated!');
     return this.store.select(AuthSelectors.selectUser).pipe(
       take(1),
-      tap(user => console.log('Current user:', user)),
       map(user => {
         if (!user || !user.plus) {
-          console.log('User is not a Plus member, redirecting...');
           this.router.navigate(['/events']);
           return false;
         }
