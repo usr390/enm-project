ee3c2305cdb3b130194dfe9b17de2ec4f546ecfa
vendingcompute <edutre1999@gmail.com>
2024-05-08 16:32:29 -0500

    x

--- enm/src/app/core/services/product-service.service.ts
+++ enm/src/app/core/services/product-service.service.ts
@@ -8,7 +8,7 @@ export class ProductService {
         return [
             {
                 name: 'Our Full Event List',
-                description: 'Currently extending through September!',
+                description: 'Currently extending through October!',
                 image: 'fulllist2.gif',
             },
             {
