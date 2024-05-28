c3510fac899fae205fe5fac566a17d572441a624
vendingcompute <edutre1999@gmail.com>
2024-05-28 00:57:39 -0500

    x

--- enm/src/app/enm-events/enm-event-list-filter/enm-event-list-filter.component.ts
+++ enm/src/app/enm-events/enm-event-list-filter/enm-event-list-filter.component.ts
@@ -38,7 +38,7 @@ export class EnmEventListFilterComponent {
   );
 
   enmEventListFilterForm = this.fb.group({ filter: '', checked: false, touring: false });
-  artistDirectoryFilterForm = this.fb.group({ filter: '', recentlyListed: false, rock: false, punk: false, metal: false, edm: false, perreo: false, techno: false, rap: false, rnb: false, jazz: false, pop: false, experimental: false, latin: false, other: false });
+  artistDirectoryFilterForm = this.fb.group({ filter: '', recentlyListed: false, Rock: false, Punk: false, Metal: false, Edm: false, Rap: false, Jazz: false, Pop: false, Experimental: false, Latin: false, Other: false });
   selectedFilterText$ = this.store$.select(EnmEventsSelectors.selectedFilterText); // for repopulating the input field after a refresh or navigation
   selectedArtistDirectoryFilterText$ = this.store$.select(ArtistDirectorySelectors.selectFilterText); // for repopulating the input field after a refresh or navigation
   filter="Just Listed"
@@ -48,7 +48,7 @@ export class EnmEventListFilterComponent {
   filter$ = this.store$.select(EnmEventsSelectors.selectFilter);
 
   ngOnInit() {
-    this.genres = ['rock', 'punk', 'metal', 'experimental', 'edm', 'rap', 'jazz', 'pop', 'latin', 'other'];
+    this.genres = ['Rock', 'Punk', 'Metal', 'Experimental', 'Edm', 'Rap', 'Jazz', 'Pop', 'Latin', 'Other'];
     this.initializeFormControl();
     this.initializeArtistDirectoryFormControl()
     this.enmEventListFilterForm.valueChanges.pipe(
@@ -80,16 +80,16 @@ export class EnmEventListFilterComponent {
   filterArtistDirectoryResults(){
     let text = this.artistDirectoryFilterForm.value.filter?.trim() as string
     let recentlyListed = this.artistDirectoryFilterForm.value.recentlyListed as boolean
-    let rock = this.artistDirectoryFilterForm.value.rock as boolean
-    let punk = this.artistDirectoryFilterForm.value.punk as boolean
-    let metal = this.artistDirectoryFilterForm.value.metal as boolean
-    let edm = this.artistDirectoryFilterForm.value.edm as boolean
-    let rap = this.artistDirectoryFilterForm.value.rap as boolean
-    let jazz = this.artistDirectoryFilterForm.value.jazz as boolean
-    let pop = this.artistDirectoryFilterForm.value.pop as boolean
-    let experimental = this.artistDirectoryFilterForm.value.experimental as boolean
-    let latin = this.artistDirectoryFilterForm.value.latin as boolean
-    let other = this.artistDirectoryFilterForm.value.other as boolean
+    let rock = this.artistDirectoryFilterForm.value.Rock as boolean
+    let punk = this.artistDirectoryFilterForm.value.Punk as boolean
+    let metal = this.artistDirectoryFilterForm.value.Metal as boolean
+    let edm = this.artistDirectoryFilterForm.value.Edm as boolean
+    let rap = this.artistDirectoryFilterForm.value.Rap as boolean
+    let jazz = this.artistDirectoryFilterForm.value.Jazz as boolean
+    let pop = this.artistDirectoryFilterForm.value.Pop as boolean
+    let experimental = this.artistDirectoryFilterForm.value.Experimental as boolean
+    let latin = this.artistDirectoryFilterForm.value.Latin as boolean
+    let other = this.artistDirectoryFilterForm.value.Other as boolean
 
     this.store$.dispatch(ArtistDirectoryActions.artistDirectoryFilter({ text, recentlyListed, rock, punk, metal, edm, rap, jazz, pop, experimental, latin, other }))
   }
