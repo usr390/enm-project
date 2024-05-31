0cb8f42880463503051048f93f053bb9bed3407d
vendingcompute <edutre1999@gmail.com>
2024-05-30 20:59:38 -0500

    x

--- api-enm/src/models/Artist.ts
+++ api-enm/src/models/Artist.ts
@@ -2,13 +2,35 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
+const LinksSchema = new Schema({
+    apple: String,
+    bandcamp: String,
+    youtube: String,
+    instagram: String,
+    spotify: String,
+    facebook: String,
+    soundcloud: String,
+    tiktok: String,
+    x: String,
+    self: String,
+    tumblr: String,
+    twitch: String
+});
+
+
+
 export const ArtistSchema = new Schema({
     name: String,
     link: String,
     location: String,
     status: String,
     debut: Boolean,
-    start: String
+    start: String,
+    medium: String,
+    links: { type: LinksSchema },
+    genre: { type: [String] },
+    end: { type: String, default: "pending" },
+    notes: { type: String, default: "" }
 });
 
 const ArtistModel = mongoose.model("Artist", ArtistSchema);
