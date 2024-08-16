819ccd4080907bf01d7391f6c24eb8b1eabd87f7
vendingcompute <edutre1999@gmail.com>
2024-08-16 14:28:55 -0500

    x

--- enm/src/app/core/blog-page/blog-page.component.ts
+++ enm/src/app/core/blog-page/blog-page.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { combineLatest, map } from 'rxjs';
 import { AppState } from 'src/app/state/app.state';
 import * as blogSelectors from './../../state/blog/blog.selectors';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,10 +13,14 @@ import * as blogSelectors from './../../state/blog/blog.selectors';
 })
 export class BlogPageComponent {
 
-  constructor(private store$: Store<AppState>,){}
+  constructor(private store$: Store<AppState>, private router: Router){}
 
   selectedBlog$ = combineLatest([this.store$.select(blogSelectors.selectAll), this.store$.select(blogSelectors.selectSelectedBlogId)]).pipe(
     map(([blogs, selectedBlog]) => blogs.filter(blog => blog._id === selectedBlog)[0]),
   );
+
+  goBack() {
+    this.router.navigate(['/blog']); 
+  }
 
 }
