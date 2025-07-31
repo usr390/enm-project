4a32a9987302c1612bbd8baf28b46457aa1c8efc
usr390 <edutre1999@gmail.com>
2025-07-31 01:08:19 -0500

    added Playwright tests

---
+++ enm/tests/mocks/mockUser.ts
@@ -0,0 +1,8 @@
+export const mockUser = {
+    "user": {
+        "id": "656f7aebc6181350249e8823",
+        "username": "test",
+        "plus": false,
+        "appAccountToken_apple": "902586a2-6b34-4f19-9681-b7ecf77502d2"
+    }
+}
